eb460c4aa5612b9005c29baa94834011dab59722
Jonathan <jonathan.graff@hotmail.fr>
2024-04-12 18:46:07 +0200

    Add files via upload

--- tests/test_calculatrice.py
+++ tests/test_calculatrice.py
@@ -1,10 +1,11 @@
 from app.calculatrice import calculate_from_str
-from app.errors import LastCaracterNotOperation, TooManyOperands, NotEnoughOperands
+from app.errors import StringInvalid
 import pytest
 
 
 @pytest.mark.parametrize(
     "calculation, answer", (
+        ("1", 1),
         ("1 2 +", 3),
         ("5.8 2.3 -", 3.5),
         ("3 2 5 * + 8 7 + +", 28),
@@ -17,13 +18,8 @@ def test_calculate_works(calculation, answer):
     assert calculate_from_str(calculation) == answer
 
 
-def test_error_last_caracter_not_an_operation():
-    with pytest.raises(LastCaracterNotOperation):
-        calculate_from_str("1 2 + 3")
-
-
 def test_error_too_many_operand():
-    with pytest.raises(TooManyOperands):
+    with pytest.raises(StringInvalid):
         calculate_from_str("1 2 3 +")
 
 
@@ -36,5 +32,5 @@ def test_error_too_many_operand():
     )
 )
 def test_error_not_enough_operand(calculation):
-    with pytest.raises(NotEnoughOperands):
+    with pytest.raises(StringInvalid):
         calculate_from_str(calculation)
